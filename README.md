4eb50192fc02a8249c7f30c8a1489cb16643a7f1
Tyler Liu <tyler4long@gmail.com>
2024-07-21 12:06:39 -0700

    react-dom createPortal

--- README.md
+++ README.md
@@ -6,5 +6,7 @@
 I find that it is hard to make markdown work with react.js.
 You need to write markdown in HTML instead of in JSX, otherwise it doesn't work.
 
+即使没有 markdown, 也是一样的现象. 需要在html中写 presentation 的内容, 而不是在 jsx 中写.
+
 - ref: https://shotstack.io/learn/use-ffmpeg-to-convert-images-to-video/
 - ref https://revealjs.com/installation/
--- src/app.tsx
+++ src/app.tsx
@@ -1,25 +1,59 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { auto } from 'manate/react';
 
 import Reveal from 'reveal.js';
 import Markdown from 'reveal.js/plugin/markdown/markdown.esm.js';
 
 import type { Store } from './store';
+import { createPortal } from 'react-dom';
 
 const App = (props: { store: Store }) => {
   useEffect(() => {
     const deck = new Reveal({
       plugins: [Markdown],
     });
-    deck.initialize();
+    const main = async () => {
+      await deck.initialize();
+      deck.sync();
+    };
+    main();
     return () => {
-      // ref: https://github.com/hakimel/reveal.js/issues/3593
+      // // ref: https://github.com/hakimel/reveal.js/issues/3593
       if (deck.isReady()) {
         deck.destroy();
       }
     };
   }, []);
-  const render = () => <div />;
+  const [counter, setCounter] = useState(0);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCounter((prev) => prev + 1);
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+  const render = () => (
+    <div>
+      {createPortal(
+        <div className="reveal">
+          <div className="slides">
+            <section data-markdown="">
+              <div data-template="">
+                {`
+    ## Slide ${counter}
+    A paragraph with some text and a [link](https://hakim.se).
+    ---
+    ## Slide 2
+    ---
+    ## Slide 3
+            `}
+              </div>
+            </section>
+          </div>
+        </div>,
+        document.body,
+      )}
+    </div>
+  );
   return auto(render, props);
 };
 
